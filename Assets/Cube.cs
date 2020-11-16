dc8969f966666085f9a85c73d981b35ca84ac63a
ClydeCarey <73868789+ClydeCarey@users.noreply.github.com>
2020-11-15 19:14:19 -1000

    Initial commit

---
+++ Assets/Cube.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    //public MeshRenderer meshRenderer;
+    //[SerializeField]
+    //public bool externalControl;
+    [SerializeField]
+    public bool _meshEnabler = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //meshRenderer.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_meshEnabler == true)
+        {
+            transform.GetComponent<MeshRenderer>().enabled = true;
+        }
+        else if (_meshEnabler == false)
+        {
+            transform.GetComponent<MeshRenderer>().enabled = false;
+        }
+    }
+}
---
+++ Assets/Sphere.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sphere : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+           // GameObject.Find("Cube").GetComponent<>().exter = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            transform.GetComponent<MeshRenderer>().enabled = false;
+        }
+    }
+}
